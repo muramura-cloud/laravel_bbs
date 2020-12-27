0944642a56304fa5c650574b2c6d3e1c376e7186
muramura-cloud <clu363721@gmail.com>
2020-12-27 20:42:07 +0900

    Add Procfile + varchar = 191

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,24 +5,15 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
     public function register()
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
     public function boot()
     {
         DB::listen(function ($query) {
@@ -32,5 +23,7 @@ class AppServiceProvider extends ServiceProvider
             }
             Log::info($sql);
         });
+
+        Schema::defaultStringLength(191);
     }
 }
